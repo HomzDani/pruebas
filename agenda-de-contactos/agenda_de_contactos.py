993d020abc5ef4d71b1bc99f7488bee060bd2d90
HomzDani <marvindanielcervantesvazquez@gmail.com>
2025-07-31 00:51:00 -0600

    agenda de contactos a3

--- agenda-de-contactos/agenda_de_contactos.py
+++ agenda-de-contactos/agenda_de_contactos.py
@@ -17,3 +17,55 @@ def buscar_contacto(agenda, nombre):
     for contacto in agenda:
         if contacto['nombre'].lower() == nombre.lower():
             return None
+def mostrar_contacto(contacto):
+    if contacto:
+        print("informacion del contacto:")
+        print(f"nombre: {contacto["nombre"]}")
+        print(f"telefono: {contacto["telefono"]}")
+        print(f"email: {contacto["email"]}")
+    else:
+        print("contacto no encontrado.")
+
+def mostrar_todos(agenda):
+    # muestra todos los contactos de la agenda
+    if not agenda:
+        print("la agenda esta vacia.")
+        return
+    
+    print("lista de contactos:")
+    for i, contactos in enumerate(agenda, 1):
+        print(f"{i}. {contacto["nombre"]} - {contacto['telefono']}")
+
+def eliminar_contacto(agenda, nombre):
+    # eliminar un contacto de la agenda
+    for i, contacto in enumerate(agenda):
+        if contacto['nombre'].lower() == nombre.lower():
+            del agenda[i]
+            print(f"contacto {nombre} eliminado correctamente.")
+            return
+    print("contacto no encontrado.")
+
+def menu():
+    # muestra el menu de opciones
+    print("Agenda de contactos")
+    print("1. agregar nuevo contacto")
+    print("2. buscar contacto")
+    print("3. mostrar todos los contactos")
+    print("4. eliminar contacto")
+    print("5. salir")
+
+def main():
+    agenda = crear_agenda()
+
+    while True:
+        menu()
+        opcion = input("seleciona una opcion: ")
+
+        if opcion == '1':
+            nombre = input("nombre: ")
+            telefono = input("telefono: ")
+            email = input("email: ")
+            agregar_contacto(agenda, nombre, telefono, email)
+
+        elif opcion == '2':
+            nombre = 
